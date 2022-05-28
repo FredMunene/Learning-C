a637e4824f94b82a516f1ae4257cf34ee4c22b75
FredMunene <munene2050gitonga@gmail.com>
2022-05-28 17:58:28 +0300

    Hello World

--- helloWorld.c
+++ helloWorld.c
@@ -3,13 +3,16 @@
 /*
 multi line comment
 */
-//escape sequence backslash \
-
+/*
+   escape sequence backslash 
+   \n =newline
+   \t = tab
+*/
 int main(){
     
 
     printf("I \nlike \npizza!\n");
-    printf("It's really good!\n");
+    printf("\"It's really good!\" - Qoute by Mogamed\n");
     printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
 
     return 0;
