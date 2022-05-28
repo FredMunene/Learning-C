dad2e97bddfca2e91e1522d34b5ce099ef16070f
FredMunene <munene2050gitonga@gmail.com>
2022-05-28 16:22:47 +0300

    initial commit

---
+++ helloWorld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//single line comment
+/*
+multi line comment
+*/
+//escape sequence backslash \
+
+int main(){
+    
+
+    printf("I \nlike \npizza!\n");
+    printf("It's really good!\n");
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
+
+    return 0;
+}
