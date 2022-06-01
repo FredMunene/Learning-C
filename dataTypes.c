6dde3706924f25dfed44d1158ff749d65a68ec3e
FredMunene <munene2050gitonga@gmail.com>
2022-06-01 19:41:34 +0300

    Learning C language concepts

--- dataTypes.c
+++ dataTypes.c
@@ -10,10 +10,25 @@ int main(){
     int j = 2435666; //4 bytes (-2,147,483,648 to +2,147,483,648)
     bool e = true; // 1 byte (true or false) %d
 
-    char f = 100; // 1 byte (-128 to +127) %d 
-    char g = 100; // 1 byte (-128 to +127) %c 
+    char f = 100; // 1 byte (-128 to +127) %d . Signed char
+    char g = 100; // 1 byte (-128 to +127) %c . signed char
+    unsigned char i = 255; // 1 byte (0 to +255) %d or %c -positive integers-
 
-    printf("\"%d\" converted to char referencing ASCII table \"%c\"\n",f,g);
+    short int j = 456; //2 bytes (-32768 to +32767)%d
+    unsigned short int k = 54000; // 2 byte (0 to 65535) %d
+    short n =32767;
+    unsigned short m = 65535;
+
+    int o = 4555; // 4 bytes (-2,471,483,648 to 2,471,483,648) % d
+    unsigned int p = 123456789; // 4 bytes (4,294,967,295) %u
+
+    long long q = 1234567890867; // 8 bytes (-9 quintillion to 9 quintillion) %lld
+    unsigned long long q = 1234567890867; // 8 bytes (-9 quintillion to 9 quintillion) %llu
+
+ 
+
+
+    //printf("\"%d\" converted to char referencing ASCII table \"%c\"\n",f,g);
 
     // printf("Pie is %f\n",c);
     // printf("Pie is %lf\n",d);
